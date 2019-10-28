08ee212c5c1817796d21bfd9533458ca7437c0d5
David Higham <davidhigham123@hotmail.com>
2019-10-28 13:28:41 +0000

    Boxes coloured by team

--- run_analysis.py
+++ run_analysis.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+from sklearn.cluster import KMeans
 import cv2
 
 import keras
@@ -79,6 +80,16 @@ def run_analysis(filename):
 
     camera_cut = CameraCut()
 
+    team_hists = np.array([[0.00200573715878, 0.008174931463, 0.01083576065,
+                            0.010703199637, 0.005602396121, 0.003398788058,
+                            0.003046975742, 0.002051472299, 0.001472585509,
+                            0.0004638160026],
+                           [0.000803887693714, 0.006247766973138,
+                            0.008450147937586, 0.01134667238931,
+                            0.004603156519655, 0.003117572623929,
+                            0.003535995531379, 0.003621568075172,
+                            0.00310307416, 0.001896991398214]])
+
     cut_counter = 1
     counter = 0
     while(cap.isOpened()):
@@ -94,31 +105,43 @@ def run_analysis(filename):
                       (int(cap.get(3)), int(cap.get(4))))
             cut_counter += 1
             counter = 0
-        draw = frame.copy()
+        draw = np.array(frame)
 
         if counter % 5 == 0:
             # Every 5 frames use retinanet to find players in the frame
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            frame = preprocess_image(frame)
-            frame, scale = resize_image(frame)
+            processed_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            processed_frame = preprocess_image(processed_frame)
+            processed_frame, scale = resize_image(processed_frame)
 
-            boxes, scores, labels = model.predict_on_batch(np.expand_dims(frame, axis=0))
+            boxes, scores, _ = model.predict_on_batch(np.expand_dims(processed_frame, axis=0))
             boxes /= scale
+            players = []
+            confident_boxes = []
+            teams = []
+            for box, score in zip(boxes[0], scores[0]):
+                if score < 0.5:
+                    break
+                box = box.astype(int)
+                player_image = draw[box[1]:box[3], box[0]:box[2], :]
+                confident_boxes.append(box)
+                player_image = cv2.cvtColor(player_image, cv2.COLOR_RGB2GRAY)
+                player_hist = np.histogram(player_image, density=True)[0]
+                dist = np.sum(np.power((team_hists - player_hist), 2), axis=1)
+                min_index = np.argmin(dist)
+                teams.append([0, 255, 0])
+                if min_index == 0 and dist[0] < 2e-05:
+                    teams[-1] = [255, 0, 0]
+                elif dist[1] < 2e-05:
+                    teams[-1] = [0, 0, 255]
 
         # Plot the most recent detections on the current frame
-        for box, score, label in zip(boxes[0], scores[0], labels[0]):
-            if score < 0.5:
-                break
-
-            color = label_color(label)
-
-            b = box.astype(int)
-            draw_box(draw, b, color=color)
-
-            caption = "{:.3f}".format(score)
-            draw_caption(draw, b, caption)
+        for box, team in zip(confident_boxes, teams):
+            draw_box(draw, box, color=team)
 
         # Write the current frame with annotations to the VideoWriter
+        # import matplotlib.pyplot as plt
+        # plt.imshow(draw)
+        # plt.show()
         out.write(draw)
         counter += 1
     cap.release()
